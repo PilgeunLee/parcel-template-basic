b8df305268885922e2b4210473c3d54ff6814e04
PilgeunLee <pgstom9015@gmail.com>
2024-01-14 12:38:40 +0900

    'createproject'

---
+++ .postcssrc.js
@@ -0,0 +1,21 @@
+//ESM
+//CommonJS
+
+// //import
+// const autoprefixer = require('autoprefixer')
+
+// //export
+
+// module.exports ={
+//     plugins: [
+//         autoprefixer
+//     ]
+
+// }
+
+
+module.exports ={
+    plugins :[
+        require('autoprefixer')
+    ]
+}
